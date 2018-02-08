2ba8ebb3378b3134f064d407de4ac6a98192ae96
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-08 11:35:58 +0000

    Remove unnecessary include in base_page

--- test_common/page_objects/base_page.rb
+++ test_common/page_objects/base_page.rb
@@ -3,8 +3,6 @@ require_relative '../messaging'
 module ET3
   module Test
     class BasePage < ::SitePrism::Page
-      include ActiveSupport::NumberHelper
-      
       # Populate
     end
   end

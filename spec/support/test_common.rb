2e691fb382a3559fc0e13c99d39aeed395459ba6
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-08 11:33:02 +0000

    Remove personas requirement from test_common.rb

--- spec/support/test_common.rb
+++ spec/support/test_common.rb
@@ -1,6 +1,5 @@
 require_relative '../../test_common/capybara'
 require_relative '../../test_common/page_objects'
-require_relative '../../test_common/personas'
 require_relative '../../test_common/messaging'
 require_relative '../../test_common/helpers'
 

3d4e56400bc4700a4560372cee9042846d0d4653
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-09 15:16:53 +0000

    Begin to load personas info There is an issue loading PersonasRepository at this stage

--- test_common/helpers/setup.rb
+++ test_common/helpers/setup.rb
@@ -1,6 +1,12 @@
 module ET3
   module Test
     module Setup
+
+      attr_accessor :user
+
+      def given_i_am(company_name)
+        self.user = personas.fetch(company_name)
+      end
     end
   end
 end

4bf01754b112c2b0e23255f7deae42464fae3e9b
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-16 13:41:26 +0000

    Use sections instead of elements and update testing suite

--- test_common/page_objects/respondents_details_page.rb
+++ test_common/page_objects/respondents_details_page.rb
@@ -3,47 +3,141 @@ module ET3
     class RespondentsDetailsPage < BasePage
       set_url '/respond/respondents_details'
 
-      element :case_number_question, :css, "#case_number"
-      element :name_question, :css, "#name_of_indiv_co_or_org"
-      element :contact_question, :css, "#name_of_contact"
-      element :building_name_question, :css, "#respondent_building_name_or_number"
-      element :street_question, :css, "#respondent_street"
-      element :town_question, :css, "#respondent_towncity"
-      element :county_question, :css, "#respondent_county"
-      element :postcode_question, :css, "#respondent_postcode"
-      element :dx_number_question, :css, "#respondent_dx_address" # DX Address?
-      element :contact_number_question, :css, "#respondent_contact_number"
-      element :contact_mobile_number_question, :css, "#respondent_mobile_number"
-      section :contact_preference_question, :css, "#question12" do |q|
-        element :preference_email, :css, '#respondents_detail_contact_preference_email'
-        element :preference_post, :css, '#respondents_detail_contact_preference_post'
-        element :preference_fax, :css, '#respondents_detail_contact_preference_fax'
-        element :email_address, :css, '#respondent_contact_email'
-        element :fax_number, :css, '#respondent_contact_fax'
+      section :case_number_question, :question_labelled, 'questions.case_number.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+
+      section :name_question, :question_labelled, 'questions.name.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+
+      section :contact_question, :question_labelled, 'questions.contact.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+
+      section :building_name_question, :question_labelled, 'questions.building_name.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+      
+      section :street_question, :question_labelled, 'questions.street.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+            
+      section :town_question, :question_labelled, 'questions.town.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+            
+      section :county_question, :question_labelled, 'questions.county.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+
+      section :postcode_question, :question_labelled, 'questions.postcode.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+            
+      section :dx_number_question, :question_labelled, 'questions.dx_number.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+            
+      section :contact_number_question, :question_labelled, 'questions.contact_number.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+            
+      section :contact_mobile_number_question, :question_labelled, 'questions.contact_mobile_number.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+
+      section :contact_preference_question, :single_choice_option, 'questions.contact_preference.label', exact: true do |q|
+
+        section :preference_email, :fieldset_question_labelled, 'questions.contact_preference.email.label' do
+          element :selector, :css, 'input[type="radio"]'
+          element :field, :css, 'input[type="text"]'
+
+          def set(value)
+            selector.set(true)
+            field.set(value)
+          end
+        end
+
+        section :preference_post, :fieldset_question_labelled, 'questions.contact_preference.post.label' do
+          element :selector, :css, 'input[type="radio"]'
+
+          delegate :set, to: :selector
+        end
+
+        section :preference_fax, :fieldset_question_labelled, 'questions.contact_preference.fax.label' do
+          element :selector, :css, 'input[type="radio"]'
+          element :field, :css, 'input[type="text"]'
+
+          def set(value)
+            selector.set(true)
+            field.set(value)
+          end
+        end
 
         def set_for(user_persona)
           case user_persona.contact_preference
             when "email"
-              preference_email.set(true)
-              email_address.set(user_persona.email_address)
+              preference_email.set(user_persona.email_address)
             when "post"
               preference_post.set(true)
             when "fax"
-              preference_fax.set(true)
-              fax_number.set(user_persona.fax_number)
+              preference_fax.set(user_persona.fax_number)
           end
         end
       end
-      element :organisation_employ_gb_question, :css, "#respondent_organisation_employ_gb"
-      section :organisation_site_number_question, :css, "#question14" do |q|
-        element :more_than_one_site, :css, '#respondents_detail_organisation_site_number_yes'
-        element :only_one_site, :css, '#respondents_detail_organisation_site_number_no'
-        element :employment_at_site_number, :css, '#respondent_employment_at_site_number'
+            
+      section :organisation_employ_gb_question, :question_labelled, 'questions.organisation_employ_gb.label', exact: true do
+        element :field, :css, "input"
+
+        delegate :set, to: :field
+      end
+      
+      section :organisation_site_number_question, :single_choice_option, 'questions.organisation_site_number.label', exact: true do |q|
+        
+        section :more_than_one_site, :fieldset_question_labelled, 'questions.organisation_site_number.more_than_one_site.label', exact: true do
+          element :selector, :css, 'input[type="radio"]'
+          element :field, :css, 'input[type="text"]'
+
+          def set(value)
+            selector.set(true)
+            field.set(value)
+          end
+        end
+
+        section :only_one_site, :fieldset_question_labelled, 'questions.organisation_site_number.only_one_site.label', exact: true do
+          element :selector, :css, 'input[type="radio"]'
+
+          delegate :set, to: :selector
+        end
 
         def set_for(user_persona)
           if user_persona.organisation_site_number > 1
-            more_than_one_site.set(true)
-            employment_at_site_number.set(user_persona.employment_at_site)
+            more_than_one_site.set(user_persona.employment_at_site)
+          else 
+            only_one_site.set(true)
           end
         end
       end

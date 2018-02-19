1b848f36c737e2432dc035249ce92046f436d0df
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-19 12:27:36 +0000

    Add tests for start page

--- spec/features/access_start_page_spec.rb
+++ spec/features/access_start_page_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Access Start Page" do
-  scenario "user will read text and begin form" do
+RSpec.feature "Access Start Page", js: true do
+  let(:respondents_details_page) { ET3::Test::RespondentsDetailsPage.new }
+
+  scenario "user will be able to read text" do
     start_page.load
 
     expect(start_page).to be_displayed
-    expect(start_page).to have_hello_world
+    expect(start_page).to have_header
+    expect(start_page).to have_introduction
+  end
+
+  scenario "user will be able to start form" do
+    start_page.load
+
+    start_page.next
+
+    expect(respondents_details_page).to be_displayed
   end
 end

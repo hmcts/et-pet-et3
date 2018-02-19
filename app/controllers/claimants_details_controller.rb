df6e09cf695ed753055c45f8cc88e95d5d589e36
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-19 17:04:29 +0000

    Build claimants_details page
    Form does not currently validate

--- app/controllers/claimants_details_controller.rb
+++ app/controllers/claimants_details_controller.rb
@@ -1,5 +1,19 @@
 class ClaimantsDetailsController < ApplicationController
   def edit
-    head :ok
+    @claimants_detail = ClaimantsDetail.new
+  end
+
+  def update
+    @claimants_detail = ClaimantsDetail.new(claimants_detail_params)
+    if @claimants_detail.valid?
+      redirect_to edit_earnings_and_benefits_path
+    end
+  end
+
+  private
+
+  def claimants_detail_params
+    params.require(:claimants_detail).permit(:claimants_name, :early_conciliation_details, :employment_dates,
+      :continued_employment, :claimants_description_of_job_or_title)
   end
 end

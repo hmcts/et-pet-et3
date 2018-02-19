7f4048085e240add16887e00b6b025146ce3f815
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-19 17:04:29 +0000

    Build claimants_details page
    Form does not currently validate

---
+++ app/forms/claimants_detail.rb
@@ -0,0 +1,7 @@
+class ClaimantsDetail < BaseForm
+  attribute :claimants_name, :string
+  attribute :early_conciliation_details, :string
+  attribute :employment_dates, :string
+  attribute :continued_employment, :string
+  attribute :claimants_description_of_job_or_title, :string
+end
---
+++ test_common/page_objects/earnings_and_benefits_page.rb
@@ -0,0 +1,7 @@
+module ET3
+  module Test
+    class EarningsAndBenefitsPage < BasePage
+      set_url '/respond/earnings_and_benefits'
+    end
+  end
+end

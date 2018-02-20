d784aa1ca3e47115434bc1dcd59b777068487afd
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-20 11:59:50 +0000

    Use booleans for radio buttons

--- app/forms/claimants_detail.rb
+++ app/forms/claimants_detail.rb
@@ -1,7 +1,7 @@
 class ClaimantsDetail < BaseForm
   attribute :claimants_name, :string
-  attribute :early_conciliation_details, :string
-  attribute :employment_dates, :string
-  attribute :continued_employment, :string
-  attribute :claimants_description_of_job_or_title, :string
+  attribute :early_conciliation_details, :boolean
+  attribute :employment_dates, :boolean
+  attribute :continued_employment, :boolean
+  attribute :claimants_description_of_job_or_title, :boolean
 end

85d997b646f19e9bb262f718cf7c013e163d9818
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-22 11:01:09 +0000

    Delete redundant capybara selector

--- test_common/capybara_selectors/fieldset_question_labelled.rb
+++
@@ -1,6 +0,0 @@
-Capybara.add_selector(:fieldset_question_labelled) do
-  xpath do |locator, _options|
-    translated = ET3::Test::Messaging.instance.translate(locator)
-    XPath.generate { |x| x.css('.multiple-choice')[x.descendant(:label)[x.string.n.is(translated)]] }
-  end
-end

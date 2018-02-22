9a80ef72db8c187ce7b3753f98e73a4c223f17d7
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-22 10:55:13 +0000

    Pass tests using upgraded testing suite

--- app/forms/claimants_detail.rb
+++ app/forms/claimants_detail.rb
@@ -1,7 +1,12 @@
 class ClaimantsDetail < BaseForm
   attribute :claimants_name, :string
   attribute :early_conciliation_details, :boolean
+  attribute :disagree_conciliation_reason, :text
   attribute :employment_dates, :boolean
+  attribute :employment_start, :datetime
+  attribute :employment_end, :datetime
+  attribute :disagree_employment, :text
   attribute :continued_employment, :boolean
   attribute :claimants_description_of_job_or_title, :boolean
+  attribute :disagree_claimants_job_or_title, :text
 end

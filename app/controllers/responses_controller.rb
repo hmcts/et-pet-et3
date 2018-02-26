d92102976839336994d98b01965c8d2f0b701675
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-26 17:23:03 +0000

    Barebones for responses page to make all tests green

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,5 @@
+class ResponsesController < ApplicationController
+  def edit
+    head :ok
+  end
+end
---
+++ test_common/page_objects/response_page.rb
@@ -0,0 +1,7 @@
+module ET3
+  module Test
+    class ResponsePage < BasePage
+      set_url '/respond/response'
+    end
+  end
+end

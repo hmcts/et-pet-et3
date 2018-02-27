82509a06691916274f504d96c282be968d712e82
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-02-27 15:15:27 +0000

    Build response page

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,5 +1,18 @@
 class ResponsesController < ApplicationController
   def edit
-    head :ok
+    @response = Response.new
+  end
+
+  def update
+    @response = Response.new(response_params)
+    if @response.valid?
+      redirect_to edit_your_representative_path
+    end
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:defend_claim, :defend_claim_facts)
   end
 end
--- test_common/page_objects/response_page.rb
+++ test_common/page_objects/response_page.rb
@@ -20,6 +20,15 @@ module ET3
         section :defend_claim_facts, :textarea_labelled, 'questions.defend_claim.defend_claim_facts.label', exact: true do
           delegate :set, to: :root_element
         end
+
+        def set_for(user_persona)
+          if user_persona.defend_claim == 'Yes'
+            yes.set(true)
+            defend_claim_facts.set(user_persona.defend_claim_facts)
+          else
+            no.set(true)
+          end
+        end
       end
 
       element :continue_button, :button, "Save and continue"

226d8e8ffd880a86399491458250265dbcaace32
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-03-08 11:06:53 +0000

    Remove binary comment as field will be a URL reference

--- app/forms/employers_contract_claim.rb
+++ app/forms/employers_contract_claim.rb
@@ -1,5 +1,5 @@
 class EmployersContractClaim < BaseForm
   attribute :make_employer_contract_claim, :boolean
   attribute :claim_information, :text
-  attribute :uploaded_content, :string # or binary?
+  attribute :uploaded_content, :string
 end

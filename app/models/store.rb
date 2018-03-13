e4136005abde498f290d78caa8759d50ed7f4dc5
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-03-13 17:29:56 +0000

    Create Store model and generate migration

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,16 @@
+require 'securerandom'
 class Store < ApplicationRecord
   attribute :uuid, :string
   serialize :hash_store
+  before_validation :generate_uuid, unless: :uuid_present?
+
+  private
+
+  def uuid_present?
+    uuid.present?
+  end
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309161735) do
+ActiveRecord::Schema.define(version: 20180313113318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180309161735) do
     t.text "hash_store"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_stores_on_uuid", unique: true
   end
 
 end

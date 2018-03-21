f08cb64b4d786b038b86f7506c457476a72cedde
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-03-21 12:30:38 +0000

    Rename table row selector

--- test_common/capybara_selectors/table_row_with_td_labelled.rb
+++ test_common/capybara_selectors/table_row_with_td_labelled.rb
@@ -1,4 +1,4 @@
-Capybara.add_selector(:table_row) do
+Capybara.add_selector(:table_row_with_td_labelled) do
   xpath do |locator, _options|
     translated = ET3::Test::Messaging.instance.translate(locator)
     XPath.generate { |x| x.descendant(:tr)[x.child(:td)[x.string.n.is(translated)]] }

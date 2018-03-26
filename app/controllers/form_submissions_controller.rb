216c6ece848e71a53a0504bf9b0b447f26f2f736
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-03-26 14:47:47 +0100

    Update to match datetime format on existing nearlive form

--- app/controllers/form_submissions_controller.rb
+++ app/controllers/form_submissions_controller.rb
@@ -2,7 +2,7 @@ class FormSubmissionsController < ApplicationController
 
   def index
     @reference_number = current_store.api_response["data"]["reference"]
-    @submission_date = current_store.api_response["data"]["submitted_at"]
+    @submission_date = Time.zone.parse(current_store.api_response["data"]["submitted_at"]).strftime('%d/%m/%Y %H:%M')
   end
 
 end

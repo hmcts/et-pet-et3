7564d58d3a65c482d86e3e855c2c90e55ded2c05
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-05 12:30:36 +0100

    Write better tests for postcode validator

--- app/validators/postcode_validator.rb
+++ app/validators/postcode_validator.rb
@@ -1,11 +1,21 @@
 require "uk_postcode"
 class PostcodeValidator < ActiveModel::EachValidator
+
+  def initialize(postcode_service: UKPostcode, **args)
+    self.postcode_service = postcode_service
+    super args
+  end
+
   def validate_each(record, attribute, value)
     if value.nil?
       record.errors.add(attribute, :blank)
     else
-      postcode = UKPostcode.parse(value)
+      postcode = postcode_service.parse(value)
       record.errors.add(attribute, :invalid) unless postcode.full_valid?
     end
   end
+
+  private
+
+  attr_accessor :postcode_service
 end
--- spec/validators/postcode_validator_spec.rb
+++ spec/validators/postcode_validator_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 RSpec.describe PostcodeValidator do
 
   let(:model_class) do
+    postcode_service = mock_postcode_service
     Class.new do
       include ActiveModel::Model
       attr_accessor :postcode
@@ -10,8 +11,35 @@ RSpec.describe PostcodeValidator do
         'MyModel'
       end
 
-      validates :postcode, postcode: true
+      validates :postcode, postcode: { postcode_service: postcode_service }
     end
+  end
+
+  let(:mock_postcode_service) { class_spy("UKPostcode", parse: mock_postcode_instance) }
+  let(:mock_postcode_instance) { double("mock_postcode_instance", full_valid?: true) } # rubocop:disable RSpec/VerifiedDoubles
+
+  it 'passes the correct data to the UKPostcode service' do
+    model = model_class.new(postcode: "WC1A AAA")
+
+    model.valid?
+
+    expect(mock_postcode_service).to have_received(:parse).with("WC1A AAA")
+  end
+
+  it 'will call the full_valid? method' do
+    model = model_class.new(postcode: "SW1H 9AJ")
+
+    model.valid?
+
+    expect(mock_postcode_instance).to have_received(:full_valid?)
+  end
+
+  it 'will not call the parse method if postcode is nil' do
+    model = model_class.new(postcode: nil)
+
+    model.valid?
+
+    expect(mock_postcode_service).not_to have_received(:parse)
   end
 
   it 'will validate a UK postcode without returning an error' do
@@ -23,6 +51,7 @@ RSpec.describe PostcodeValidator do
   end
 
   it 'will return an error when an invalid postcode is entered' do
+    allow(mock_postcode_instance).to receive(:full_valid?).and_return(false)
     model = model_class.new(postcode: "not a postcode")
 
     model.valid?

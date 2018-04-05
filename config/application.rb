ed55499005b508176646c2b890e8a29a022635d4
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-05 10:33:10 +0100

    Install and configure Govuk form builder

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Et3
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ActionView::Base.default_form_builder = GovukElementsFormBuilder::FormBuilder
   end
 end

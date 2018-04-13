a84efbd787eaad1075fbe1473c695fd1a324044a
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-13 11:18:56 +0100

    Add error elements to CD page and set exact text match to false

--- test_common/page_objects/claimants_details_page.rb
+++ test_common/page_objects/claimants_details_page.rb
@@ -3,27 +3,32 @@ module ET3
     class ClaimantsDetailsPage < BasePage
       set_url '/respond/claimants_details'
 
-      section :claimants_name_question, :question_labelled, 'questions.claimants_name.label', exact: true do
+      element :error_header, :error_titled, 'errors.header', exact: true
+
+      section :claimants_name_question, :question_labelled, 'questions.claimants_name.label', exact: false do
         element :field, :css, "input"
+        element :error_contains_numbers, :exact_error_text, 'errors.messages.contains_numbers', exact: false
+        element :error_contains_no_spaces, :exact_error_text, 'errors.messages.contains_no_spaces', exact: false
+
 
         delegate :set, to: :field
       end
 
-      section :agree_with_early_conciliation_details_question, :single_choice_option, 'questions.agree_with_early_conciliation_details.label', exact: true do
+      section :agree_with_early_conciliation_details_question, :single_choice_option, 'questions.agree_with_early_conciliation_details.label', exact: false do
         
-        section :yes, :gds_multiple_choice_option, 'questions.agree_with_early_conciliation_details.yes.label', exact: true do
+        section :yes, :gds_multiple_choice_option, 'questions.agree_with_early_conciliation_details.yes.label', exact: false do
           element :selector, :css, 'input[type="radio"]'
 
           delegate :set, to: :selector
         end
         
-        section :no, :gds_multiple_choice_option, 'questions.agree_with_early_conciliation_details.no.label', exact: true do
+        section :no, :gds_multiple_choice_option, 'questions.agree_with_early_conciliation_details.no.label', exact: false do
           element :selector, :css, 'input[type="radio"]'
           
           delegate :set, to: :selector          
         end
 
-        section :disagree_conciliation_reason, :textarea_labelled, 'questions.agree_with_early_conciliation_details.disagree_conciliation_reason.label', exact: true do
+        section :disagree_conciliation_reason, :textarea_labelled, 'questions.agree_with_early_conciliation_details.disagree_conciliation_reason.label', exact: false do
           delegate :set, to: :root_element
         end
 
@@ -37,55 +42,64 @@ module ET3
         end
       end
 
-      section :agree_with_employment_dates_question, :single_choice_option, 'questions.agree_with_employment_dates.label', exact: true do
+      section :agree_with_employment_dates_question, :single_choice_option, 'questions.agree_with_employment_dates.label', exact: false do
         
-        section :yes, :gds_multiple_choice_option, 'questions.agree_with_employment_dates.yes.label', exact: true do
+        section :yes, :gds_multiple_choice_option, 'questions.agree_with_employment_dates.yes.label', exact: false do
           element :selector, :css, 'input[type="radio"]'
 
           delegate :set, to: :selector
         end
 
-        section :no, :gds_multiple_choice_option, 'questions.agree_with_employment_dates.no.label', exact: true do
+        section :no, :gds_multiple_choice_option, 'questions.agree_with_employment_dates.no.label', exact: false do
           element :selector, :css, 'input[type="radio"]'
 
           delegate :set, to: :selector
         end
 
-        section :employment_start, :question_labelled, 'questions.agree_with_employment_dates.employment_start.label', exact: true do
-          element :field, :css, 'input[type="date"]'
+        section :employment_start, :question_labelled, 'questions.agree_with_employment_dates.employment_start.label', exact: false do
+          element :field, :css, 'input'
+          element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+
 
           delegate :set, to: :field
         end
 
-        section :employment_end, :question_labelled, 'questions.agree_with_employment_dates.employment_end.label', exact: true do
-          element :field, :css, 'input[type="date"]'
+        section :employment_end, :question_labelled, 'questions.agree_with_employment_dates.employment_end.label', exact: false do
+          element :field, :css, 'input'
+          element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+
 
           delegate :set, to: :field
         end
 
-        section :disagree_employment, :textarea_labelled, 'questions.agree_with_employment_dates.disagree_employment.label', exact: true do
-          delegate :set, to: :root_element
+        section :disagree_employment, :question_labelled, 'questions.agree_with_employment_dates.disagree_employment.label', exact: false do
+          element :field, :css, 'textarea'
+          element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+          
+          delegate :set, to: :field
         end
+
+        element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
 
         def set_for(user_persona)
           if user_persona.agree_with_employment_dates == 'No'
             no.set(true)
-            employment_start.set(user_persona.employment_start)
-            employment_end.set(user_persona.employment_end)
-            disagree_employment.set(user_persona.disagree_employment)
+            employment_start.set(user_persona.employment_start) unless user_persona.employment_start == nil
+            employment_end.set(user_persona.employment_end) unless user_persona.employment_end == nil
+            disagree_employment.set(user_persona.disagree_employment) unless user_persona.disagree_employment == nil
           else
             yes.set(true)
           end
         end
       end
 
-      section :continued_employment_question, :single_choice_option, 'questions.continued_employment.label', exact: true do
+      section :continued_employment_question, :single_choice_option, 'questions.continued_employment.label', exact: false do
         element :field, :css, "input"
 
         delegate :set, to: :field
       end
 
-      section :agree_with_claimants_description_of_job_or_title_question, :single_choice_option, 'questions.agree_with_claimants_description_of_job_or_title.label', exact: true do
+      section :agree_with_claimants_description_of_job_or_title_question, :single_choice_option, 'questions.agree_with_claimants_description_of_job_or_title.label', exact: false do
         
         section :yes, :gds_multiple_choice_option, 'questions.agree_with_claimants_description_of_job_or_title.yes.label' do
           element :selector, :css, "input"
@@ -99,7 +113,7 @@ module ET3
           delegate :set, to: :selector
         end
 
-        section :disagree_claimants_job_or_title, :textarea_labelled, 'questions.agree_with_claimants_description_of_job_or_title.disagree_claimants_job_or_title.label', exact: true do
+        section :disagree_claimants_job_or_title, :textarea_labelled, 'questions.agree_with_claimants_description_of_job_or_title.disagree_claimants_job_or_title.label', exact: false do
           delegate :set, to: :root_element
         end
 

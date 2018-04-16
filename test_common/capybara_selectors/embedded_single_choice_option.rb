c8394609c4072c9ab96880250b6681c7bc402dd4
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-16 17:43:51 +0100

    Update selector to select a div at a higher level on EandB page

--- test_common/capybara_selectors/embedded_single_choice_option.rb
+++ test_common/capybara_selectors/embedded_single_choice_option.rb
@@ -1,6 +1,6 @@
 Capybara.add_selector(:embedded_single_choice_option) do
   xpath do |locator, _options|
     translated = ET3::Test::Messaging.instance.translate(locator)
-    XPath.generate { |x| x.descendant(:div)[x.child(:fieldset)[x.child(:legend)[x.string.n.is(translated)]]] }
+    XPath.generate { |x| x.descendant(:div)[x.child(:div)[x.child(:fieldset)[x.child(:legend)[x.string.n.is(translated)]]]] }
   end
 end

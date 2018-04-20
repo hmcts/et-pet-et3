78fc68ea3034c01475e78f88d907c0e0f7278144
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-20 11:01:01 +0100

    Create answer_all_questions test method and refactor relevant specs

--- spec/features/access_form_submission_page_spec.rb
+++ spec/features/access_form_submission_page_spec.rb
@@ -2,8 +2,27 @@ require 'rails_helper'
 
 RSpec.feature "Access Form Submission Page", js: true do
 
+  before do
+    stub_request(:post, "https://et-api-example.com/v2/repondents/response").
+      with(headers: { content_type: 'application/json', 'Accept': 'application/json' }).
+      to_return(
+        headers: { content_type: 'application/json' },
+        body:
+          {
+            "data": {
+              "reference": "992000000100",
+              "submitted_at": "2018-01-13 14:00",
+              "pdf": "s3/link/to/form/pdf"
+            }
+          }.to_json,
+        status: 201
+      )
+  end
+
   scenario "user will be able to read text" do
-    form_submission_page.load
+    given_i_am(:company01)
+    answer_all_questions
+    confirmation_of_supplied_details_page.submit_form
 
     expect(form_submission_page).to be_displayed
 
@@ -13,9 +32,11 @@ RSpec.feature "Access Form Submission Page", js: true do
   end
 
   scenario "user can navigate to the gov.uk homepage" do
-    form_submission_page.load
+    given_i_am(:company01)
+    answer_all_questions
+    confirmation_of_supplied_details_page.submit_form
 
-    form_submission_page.finish
+    form_submission_page.return
 
     expect(current_url).to eql 'https://www.gov.uk/'
   end
--- spec/features/fill_in_confirmation_of_supplied_details_page_spec.rb
+++ spec/features/fill_in_confirmation_of_supplied_details_page_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 RSpec.feature "Fill in Confirmation of Supplied Details Page", js: true do
   let(:form_submission_page) { ET3::Test::FormSubmissionPage.new }
 
+  before do
+    stub_request(:post, "https://et-api-example.com/v2/repondents/response").
+      with(headers: { content_type: 'application/json', 'Accept': 'application/json' }).
+      to_return(
+        headers: { content_type: 'application/json' },
+        body:
+          {
+            "data": {
+              "reference": "992000000100",
+              "submitted_at": "2018-01-13 14:00",
+              "pdf": "s3/link/to/form/pdf"
+            }
+          }.to_json,
+        status: 201
+      )
+  end
+
   scenario "correctly will enable user to continue to next page" do
-    confirmation_of_supplied_details_page.load
-
     given_i_am(:company01)
-
-    answer_email_receipt_question
-    answer_confirm_email_receipt_question
+    answer_all_questions
 
     confirmation_of_supplied_details_page.submit_form
 

ac693a69d56175567f234af21f87f144f8b74c83
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-25 14:54:46 +0100

    Update page object and related test elements

--- test_common/page_objects/your_representatives_details_page.rb
+++ test_common/page_objects/your_representatives_details_page.rb
@@ -3,9 +3,11 @@ module ET3
     class YourRepresentativesDetailsPage < BasePage
       set_url '/respond/your_representatives_details'
 
-      section :type_of_representative_question, :single_choice_option, 'questions.type_of_representative.label', exact: true do
-        include ET3::Test::I18n
+      element :error_header, :error_titled, 'errors.header', exact: true
 
+      section :type_of_representative_question, :single_choice_option, 'questions.type_of_representative.label', exact: false do
+        include ET3::Test::I18n
+        
         section :citizens_advice_bureau, :gds_multiple_choice_option, 'questions.type_of_representative.citizens_advice_bureau.label', exact: true do
           element :selector, :css, 'input'
 
@@ -54,6 +56,8 @@ module ET3
           delegate :set, to: :selector
         end
 
+        element :error_inclusion, :exact_error_text, 'errors.messages.inclusion', exact: false
+
         def set_for(user_persona)
           case user_persona.type_of_representative
             when t('questions.type_of_representative.citizens_advice_bureau.label')
@@ -76,79 +80,87 @@ module ET3
         end
       end
 
-      section :representative_org_name_question, :question_labelled, 'questions.representative_org_name.label', exact: true do
+      section :representative_org_name_question, :question_labelled, 'questions.representative_org_name.label', exact: false do
         element :field, :css, "input"
 
         delegate :set, to: :field
       end
 
-      section :representative_name_question, :question_labelled, 'questions.representative_name.label', exact: true do
+      section :representative_name_question, :question_labelled, 'questions.representative_name.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_contains_numbers, :exact_error_text, 'errors.messages.contains_numbers', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_building_question, :question_labelled, 'questions.representative_building.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_street_question, :question_labelled, 'questions.representative_street.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_town_question, :question_labelled, 'questions.representative_town.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_blank, :exact_error_text, 'errors.messages.blank', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_county_question, :question_labelled, 'questions.representative_county.label', exact: false do
         element :field, :css, "input"
 
         delegate :set, to: :field
       end
 
-      section :representative_building_question, :question_labelled, 'questions.representative_building.label', exact: true do
+      section :representative_postcode_question, :question_labelled, 'questions.representative_postcode.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_invalid, :exact_error_text, 'errors.messages.invalid', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_phone_question, :question_labelled, 'questions.representative_phone.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_invalid, :exact_error_text, 'errors.messages.invalid', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_mobile_question, :question_labelled, 'questions.representative_mobile.label', exact: false do
+        element :field, :css, "input"
+
+        element :error_invalid, :exact_error_text, 'errors.messages.invalid', exact: false
+
+        delegate :set, to: :field
+      end
+
+      section :representative_dx_number_question, :question_labelled, 'questions.representative_dx_number.label', exact: false do
         element :field, :css, "input"
 
         delegate :set, to: :field
       end
 
-      section :representative_street_question, :question_labelled, 'questions.representative_street.label', exact: true do
+      section :representative_reference_question, :question_labelled, 'questions.representative_reference.label', exact: false do
         element :field, :css, "input"
 
         delegate :set, to: :field
       end
 
-      section :representative_town_question, :question_labelled, 'questions.representative_town.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_county_question, :question_labelled, 'questions.representative_county.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_postcode_question, :question_labelled, 'questions.representative_postcode.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_postcode_question, :question_labelled, 'questions.representative_postcode.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_phone_question, :question_labelled, 'questions.representative_phone.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_mobile_question, :question_labelled, 'questions.representative_mobile.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_dx_number_question, :question_labelled, 'questions.representative_dx_number.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_reference_question, :question_labelled, 'questions.representative_reference.label', exact: true do
-        element :field, :css, "input"
-
-        delegate :set, to: :field
-      end
-
-      section :representative_contact_preference_question, :single_choice_option, 'questions.representative_contact_preference.label', exact: true do
+      section :representative_contact_preference_question, :single_choice_option, 'questions.representative_contact_preference.label', exact: false do
         
         section :select_email, :gds_multiple_choice_option, 'questions.representative_contact_preference.email.label' do
           element :selector, :css, 'input[type="radio"]'
@@ -176,6 +188,8 @@ module ET3
           delegate :set, to: :root_element
         end
 
+        element :error_invalid, :exact_error_text, 'errors.messages.invalid', exact: false
+
         def set_for(user_persona)
           case user_persona.representative_contact_preference
             when "Email"
@@ -190,7 +204,7 @@ module ET3
         end
       end
 
-      section :representative_disability_question, :single_choice_option, 'questions.representative_disability.label', exact: true do
+      section :representative_disability_question, :single_choice_option, 'questions.representative_disability.label', exact: false do
 
         section :yes, :gds_multiple_choice_option, 'questions.representative_disability.yes.label', exact: true do
           element :selector, :css, 'input'
@@ -204,9 +218,11 @@ module ET3
           delegate :set, to: :selector
         end
 
-        section :representative_disability_information, :textarea_labelled, 'questions.representative_disability.disability_information.label', exact: :true do
+        section :representative_disability_information, :textarea_labelled, 'questions.representative_disability.disability_information.label', exact: :false do
           delegate :set, to: :root_element
         end
+
+        element :error_too_long, :exact_error_text, 'errors.messages.too_long', exact: false
 
         def set_for(user_persona)
           if user_persona.representative_disability == "Yes"

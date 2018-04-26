9781eb5752eb741b63fb2a1c6a3e281cd42ce9d3
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-04-26 15:15:00 +0100

    Update specs with erroneous flow

--- spec/features/fill_in_employers_contract_claim_page_spec.rb
+++ spec/features/fill_in_employers_contract_claim_page_spec.rb
@@ -4,8 +4,19 @@ RSpec.feature "Fill in Employers Contract Claim Page", js: true do
 
   scenario "correctly will enable user to continue to next page" do
     given_i_am(:company01)
-    answer_all_questions
+
+    answer_all_to_employers_contract_claim
+    employers_contract_claim_page.next
 
     expect(confirmation_of_supplied_details_page).to be_displayed
+  end
+
+  scenario "incorrectly will provide errors" do
+    given_i_am(:erroneously_entering_data_on_employers_contract_claim_page)
+    answer_all_to_employers_contract_claim
+    employers_contract_claim_page.next
+
+    expect(employers_contract_claim_page).to have_error_header
+    expect(employers_contract_claim_page.make_employer_contract_claim_question).to have_error_too_long
   end
 end
--- spec/forms/employers_contract_claim_spec.rb
+++ spec/forms/employers_contract_claim_spec.rb
@@ -1,45 +1,68 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe EmployersContractClaim, type: :model do
 
+  let(:four_thousand_five_hundred_and_one_characters) { Faker::Lorem.characters(4501) }
+
+  let(:populated_employers_contract_claim) {
+    described_class.new(
+      make_employer_contract_claim: true, claim_information: 'lorem ipsum claim'
+    )
+  }
+
+  context 'with validators' do
+    it 'will not validate claim_information over 4,500 characters' do
+      populated_employers_contract_claim.claim_information = four_thousand_five_hundred_and_one_characters
+
+      populated_employers_contract_claim.valid?
+
+      expect(populated_employers_contract_claim.errors.details[:claim_information]).to include a_hash_including(error: :too_long)
+    end
+  end
+
   context 'when correctly populated' do
 
-    it 'with make_employer_contract_claim only' do
-      employers_contract_claim = described_class.new(make_employer_contract_claim: true)
-
-      expect(employers_contract_claim.make_employer_contract_claim).to be true
+    it 'returns the correct make_employer_contract_claim' do
+      expect(populated_employers_contract_claim.make_employer_contract_claim).to be true
     end
 
-    it 'with claim_information only' do
-      employers_contract_claim = described_class.new(claim_information: 'lorem ipsum claim')
-
-      expect(employers_contract_claim.claim_information).to eql 'lorem ipsum claim'
+    it 'returns the correct claim_information' do
+      expect(populated_employers_contract_claim.claim_information).to eql 'lorem ipsum claim'
     end
-
-    # TODO: Unit tests for upload functionality
 
   end
 
   describe ".to_h " do
-    it "will take two strings and convert them into a hash" do
-      employers_contract_claim = described_class.new(make_employer_contract_claim: true, claim_information: 'lorem ipsum claim', upload_additional_information: 'path/to/file.rtf')
-
-      expect(employers_contract_claim.to_h).to be_a(Hash)
+    it "will return a hash" do
+      expect(populated_employers_contract_claim.to_h).to be_a(Hash)
     end
 
     it 'will return the make_employer_contract_claim key and value pair' do
-      employers_contract_claim = described_class.new(make_employer_contract_claim: true)
-
-      expect(employers_contract_claim.to_h).to include(make_employer_contract_claim: true)
+      expect(populated_employers_contract_claim.to_h).to include(make_employer_contract_claim: true)
     end
 
     it 'will return the claim_information key and value pair' do
-      employers_contract_claim = described_class.new(claim_information: 'lorem ipsum claim info')
-
-      expect(employers_contract_claim.to_h).to include(claim_information: 'lorem ipsum claim info')
+      expect(populated_employers_contract_claim.to_h).to include(claim_information: 'lorem ipsum claim')
     end
 
-    # TODO: Unit test for upload functionality in hash_store
+  end
 
+  context 'when left blank' do
+    it 'will not raise a validation error on make_employer_contract_claim' do
+      populated_employers_contract_claim.make_employer_contract_claim = nil
+
+      populated_employers_contract_claim.valid?
+
+      expect(populated_employers_contract_claim).to be_valid
+    end
+
+    it 'will not raise a validation error on claim_information' do
+      populated_employers_contract_claim.claim_information = nil
+
+      populated_employers_contract_claim.valid?
+
+      expect(populated_employers_contract_claim).to be_valid
+    end
   end
 end

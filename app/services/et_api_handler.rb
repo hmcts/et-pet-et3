47fa25b5e055ccccfceed0375669718d0d7e2b0d
Gary Taylor <gary.taylor@hismessages.com>
2018-05-07 12:59:37 +0100

    ET3 API Handler now fetches its URL from the environment (defaulting to the original value) Renamed spelling mistake 'repondents' to 'respondents' in api URL The base URL for all page objects is now prefixed with the value of ENV['ET3_URL'] to assist in docker setups

--- app/services/et_api_handler.rb
+++ app/services/et_api_handler.rb
@@ -8,7 +8,7 @@ class EtApiHandler
     # The line below flattens this while retaining the hash structure
     one_dimensional_hash = form_hash.values.inject(&:merge)
 
-    http_response = HTTParty.post("https://et-api-example.com/v2/repondents/response",
+    http_response = HTTParty.post("#{ENV.fetch('ET_API_URL', 'https://et-api-example.com')}/v2/respondents/response",
       body: one_dimensional_hash.to_json,
       headers: { content_type: 'application/json', 'Accept': 'application/json' })
 
--- test_common/page_objects/base_page.rb
+++ test_common/page_objects/base_page.rb
@@ -3,6 +3,9 @@ require_relative '../messaging'
 module ET3
   module Test
     class BasePage < ::SitePrism::Page
+      def self.set_url(url)
+        super "#{ENV['ET3_URL']}#{url}"
+      end
       # Populate
     end
   end

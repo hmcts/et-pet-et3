79a878e656788d4b2f23d4574419817d17d5a91e
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-05-14 17:30:43 +0100

    Add data loading spec for resp_det page

--- spec/features/fill_in_respondents_details_page_spec.rb
+++ spec/features/fill_in_respondents_details_page_spec.rb
@@ -56,4 +56,51 @@ RSpec.feature "Fill in Respondents Details Page", js: true do
     expect(respondents_details_page.organisation_employ_gb_question).to have_error_not_a_number
     expect(respondents_details_page.organisation_more_than_one_site_question).to have_error_not_a_number
   end
+
+  scenario "correctly will enable user to check answers and return to edit them" do
+    respondents_details_page.load
+ 
+    given_i_am(:company01)
+ 
+    answer_case_number_question
+    answer_name_question
+    answer_contact_question
+    answer_building_name_question
+    answer_street_question
+    answer_town_question
+    answer_county_question
+    answer_postcode_question
+    answer_dx_number_question
+    answer_contact_number_question
+    answer_contact_mobile_number_question
+    answer_contact_preference_question
+    answer_organisation_employ_gb_question
+    answer_organisation_more_than_one_site_question
+ 
+    respondents_details_page.next
+    visit confirmation_of_supplied_details_path
+    confirmation_of_supplied_details_page.confirmation_of_respondents_details_answers.edit_page_link.click
+ 
+    expect(respondents_details_page).to be_displayed
+    expect(respondents_details_page.case_number_question.field.value).to eql user.case_number
+    expect(respondents_details_page.name_question.field.value).to eql user.name
+    expect(respondents_details_page.contact_question.field.value).to eql user.contact
+    expect(respondents_details_page.building_name_question.field.value).to eql user.building_name
+    expect(respondents_details_page.street_question.field.value).to eql user.street_name
+    expect(respondents_details_page.town_question.field.value).to eql user.town
+    expect(respondents_details_page.county_question.field.value).to eql user.county
+    expect(respondents_details_page.postcode_question.field.value).to eql user.postcode
+    expect(respondents_details_page.dx_number_question.field.value).to eql user.dx_number
+    expect(respondents_details_page.contact_number_question.field.value).to eql user.contact_number
+    expect(respondents_details_page.contact_mobile_number_question.field.value).to eql user.contact_mobile_number
+    expect(respondents_details_page.contact_preference_question.select_email.has_checked_field?).to be true if user.contact_preference == "Email"
+    expect(respondents_details_page.contact_preference_question.preference_email.value).to eql user.email_address if user.contact_preference == "Email"
+    expect(respondents_details_page.contact_preference_question.select_post.has_checked_field?).to be true if user.contact_preference == "Post"
+    expect(respondents_details_page.contact_preference_question.select_fax.has_checked_field?).to be true if user.contact_preference == "Fax"
+    expect(respondents_details_page.contact_preference_question.preference_fax.value).to eql user.fax_number if user.contact_preference == "Fax"
+    expect(respondents_details_page.organisation_employ_gb_question.field.value.to_i).to eql user.organisation_employ_gb
+    expect(respondents_details_page.organisation_more_than_one_site_question.yes.has_checked_field?).to be true if user.organisation_more_than_one_site == "Yes"
+    expect(respondents_details_page.organisation_more_than_one_site_question.employment_at_site_number.value).to eql user.employment_at_site_number if user.organisation_more_than_one_site == "Yes"
+    expect(respondents_details_page.organisation_more_than_one_site_question.no.has_checked_field?).to be true if user.organisation_more_than_one_site == "No"
+  end
 end

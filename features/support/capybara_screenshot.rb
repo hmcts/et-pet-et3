85a4fdbfdd9a9c1098dcf71d0838f12bd4b42502
AlexaVB <Alexa.Ballantine@HMCTS.NET>
2018-06-13 15:39:14 +0100

    RST-1035 removed cucumber tests from et3 as they are now on et-full-system (#44)

--- features/support/capybara_screenshot.rb
+++
@@ -1,5 +0,0 @@
-require 'capybara-screenshot/cucumber'
-Capybara::Screenshot.prune_strategy = :keep_last_run
-Capybara::Screenshot.register_driver(:chrome) do |driver, path|
-  driver.browser.save_screenshot(path)
-end

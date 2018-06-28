d4ed44f26a19866a94a3ab2a5ac17ae62c531afa
Efstathios Stivaros <efstathios.stivaros@solirius.com>
2018-06-28 11:31:00 +0100

    Add office address, phone number and pdf url to submission page

--- app/controllers/form_submissions_controller.rb
+++ app/controllers/form_submissions_controller.rb
@@ -3,6 +3,9 @@ class FormSubmissionsController < ApplicationController
   def index
     @reference_number = current_store.api_response["meta"]["BuildResponse"]["reference"]
     @submission_date = Time.zone.parse(current_store.api_response["meta"]["BuildResponse"]["submitted_at"]).strftime('%d/%m/%Y %H:%M')
+    @pdf_url = current_store.api_response["meta"]["BuildResponse"]["pdf_url"]
+    @office_address = current_store.api_response["meta"]["BuildResponse"]["office_address"]
+    @office_phone_number = current_store.api_response["meta"]["BuildResponse"]["office_phone_number"]
   end
 
 end

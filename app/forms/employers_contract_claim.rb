1f8437eae56a186f607025fd2f147ab7cc173f6f
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-07-23 17:32:58 +0100

    Prevent conditional information from being submitted
    If there is an input field under a radio button, the content of that field is no longer submitted or validated unless the radio button is active.

--- app/forms/employers_contract_claim.rb
+++ app/forms/employers_contract_claim.rb
@@ -3,15 +3,25 @@ class EmployersContractClaim < BaseForm
   attribute :claim_information, :text
 
   def to_h
-    {
-      make_employer_contract_claim: make_employer_contract_claim,
-      claim_information: claim_information
+    employer_contract_claim_hash = {
+      make_employer_contract_claim: make_employer_contract_claim
     }
+
+    employer_contract_claim_hash.merge!(claim_information: claim_information) if employer_contract_claim_hash[:make_employer_contract_claim]
+
+    employer_contract_claim_hash
   end
 
   validates :claim_information,
     length: {
       maximum: 4500,
       too_long: "%{count} characters is the maximum allowed" # rubocop:disable Style/FormatStringToken
-    }
+    },
+    if: :make_claim?
+
+  private
+
+  def make_claim?
+    make_employer_contract_claim
+  end
 end

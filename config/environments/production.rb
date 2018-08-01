40aa4021078b730e5cb90795051066d915468a3f
Gary Taylor <gary.taylor@hismessages.com>
2018-08-01 15:39:25 +0100

    Added sentry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Raven.configure do |config|
+    config.dsn = ENV.fetch('RAVEN_DSN', '')
+  end
 end

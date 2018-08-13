70ec68d27f86b865d5ce55b4453e364de040b5f3
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-08-13 14:26:20 +0100

    Remove dotenv boot references entirely

--- config/boot.rb
+++ config/boot.rb
@@ -2,4 +2,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-require 'dotenv/load' if ENV['RUBY_ENV'] == "development" || ENV['RUBY_ENV'] == "test" # Use dotenv gem to load .env file variables into environment.

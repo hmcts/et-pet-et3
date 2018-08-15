3d74b1216e04e8e85742b83def23cfce47e310ef
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-08-15 11:26:16 +0100

    Truncate currency_floats beyond 2dp

--- app/types/currency_float_type.rb
+++ app/types/currency_float_type.rb
@@ -1,10 +1,10 @@
 class CurrencyFloatType < ActiveModel::Type::Float
-  CURRENCY_REGEX = /\A\s*\d*.\d{0,2}\s*\z/
+  CURRENCY_REGEX = /\A\s*\d*.\d*\s*\z/
 
   def cast(value)
     return value if value.is_a?(Integer)
     value.delete!('£,') if value.is_a?(String)
     return value if value.is_a?(String) && !CURRENCY_REGEX.match?(value)
-    super
+    super.truncate(2)
   end
 end

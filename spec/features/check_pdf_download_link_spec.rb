ccb1006c3bbde0184c762f6bc0932d774f649308
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-09-12 16:45:09 +0100

    Add comments to explain the use of a custom controller in a spec

--- spec/features/check_pdf_download_link_spec.rb
+++ spec/features/check_pdf_download_link_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "Check PDF Download Link", js: true do
 
+  # Upon ET3 submission to API, the API generates a PDF version of the ET3 form it just received.
+  # In the API's response to ET3's submission, it returns a URL for this PDF. However, due to
+  # the asynchronous nature of the PDF generation and API response, the file may not be ready
+  # by the time an ET3 user sees the form submission page.
+  #
+  # As such, upon the form submission page loading, ET3 has a javascript function which checks
+  # whether the PDF link is valid. If it is invalid, it disables the link and changes the text
+  # displayed to the user. It will then check the link every 10 seconds until the link is
+  # valid.
+  #
+  # This spec test this functionality, therefore it uses a special stub which returns a pdf
+  # URL of '/test/pdf-download'. This utilises the controller below to return an invalid
+  # link the first time of asking and a valid one thereafter.
+
   class PdfDownloadLinkController < ActionController::Base
     def show
       session[:hit_count] ||= 0

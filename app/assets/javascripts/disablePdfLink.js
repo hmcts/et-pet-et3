000f36c16c09c82af5a7d05abdf0b9260771fce6
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-11-29 19:57:11 +0000

    Pass tests in Welsh on CoSD page
    Use i18n strings instead of hard coded English on tables.
    
    Use two anchors for the pdf download link to enable i18n via Ruby.

--- app/assets/javascripts/disablePdfLink.js
+++ app/assets/javascripts/disablePdfLink.js
@@ -1,22 +1,22 @@
 $( document ).on('turbolinks:load', function() {
-    if ($('.download-pdf').length) {
+    if ($('.pdf-success').length) {
         checkLinkStatus();
     }
 });
 
 function checkLinkStatus() {
-    let url = $('.download-pdf').attr('href');
+    let url = $('.pdf-success').attr('href');
     let http = new XMLHttpRequest();
     http.overrideMimeType("application/pdf");
     http.open('GET', url, true);
     http.onload = function (e) {
         if (http.readyState === 4) {
             if (http.status === 200) {
-                $('.download-pdf').text('A PDF version of your completed form is now available for download')
-                    .css('pointer-events', 'auto');
+                $('.pdf-failure').addClass("hidden");
+                $('.pdf-success').removeClass("hidden").css('pointer-events', 'auto');
             } else {
-                $('.download-pdf').text('Not quite ready yet - a PDF version of your completed form will be available for download shortly')
-                    .css('pointer-events', 'none');
+                $('.pdf-success').addClass("hidden");
+                $('.pdf-failure').removeClass("hidden").css('pointer-events', 'none');
                 console.warn(`Unable to find PDF, retrying ${url} in 10 seconds`);
                 setTimeout(function() { checkLinkStatus() }, 10000);
             }

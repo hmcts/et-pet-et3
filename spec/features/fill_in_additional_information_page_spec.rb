0a581a1947fdef3cf19a4a43e066fdf6a42a634f
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-12-05 11:44:43 +0000

    Pass tests on Additional Info page

--- spec/features/fill_in_additional_information_page_spec.rb
+++ spec/features/fill_in_additional_information_page_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Fill in Additional Information Page", js: true do
   end
 
   scenario "correctly will enable user to continue to next page" do
-    additional_information_page.load
+    additional_information_page.load(locale: current_locale_parameter)
 
     given_i_am(:company01)
 
@@ -20,14 +20,14 @@ RSpec.feature "Fill in Additional Information Page", js: true do
   end
 
   scenario "without uploading a file will not display 'Remove file' link on CoSD page" do
-    additional_information_page.load
+    additional_information_page.load(locale: current_locale_parameter)
     additional_information_page.next
 
     expect(confirmation_of_supplied_details_page.confirmation_of_additional_information_answers).not_to have_link(t('components.confirmation_of_supplied_details.remove_file_link'), href: remove_rtf_path)
   end
 
   scenario "correctly will leave a file in the s3 bucket" do
-    additional_information_page.load
+    additional_information_page.load(locale: current_locale_parameter)
 
     given_i_am(:company01)
 

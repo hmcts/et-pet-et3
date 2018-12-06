513c152b21d748f713e660bb9e763b282fe64b29
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-12-06 11:47:11 +0000

    Rewrite landing page spec to prepare for Welsh

--- spec/features/access_start_page_spec.rb
+++ spec/features/access_start_page_spec.rb
@@ -6,7 +6,26 @@ RSpec.feature "Access Start Page", js: true do
   scenario "user will be able to read text" do
     start_page.load(locale: current_locale_parameter)
 
-    expect(start_page).to be_displayed
+    expect(start_page).to have_header
+    expect(start_page).to have_description
+
+    expect(start_page.what_you_need).to have_header
+    expect(start_page.what_you_need).to have_bullet_one
+    expect(start_page.what_you_need).to have_bullet_two
+    expect(start_page.what_you_need).to have_bullet_three
+    expect(start_page.what_you_need).to have_bullet_four
+    expect(start_page.what_you_need).to have_bullet_five
+    expect(start_page.what_you_need).to have_bullet_six
+
+    expect(start_page.how_to_fill).to have_header
+    expect(start_page.how_to_fill).to have_bullet_one
+    expect(start_page.how_to_fill).to have_bullet_two
+    expect(start_page.how_to_fill).to have_bullet_three
+
+    expect(start_page.dpa).to have_header
+    expect(start_page.dpa).to have_content
+
+    expect(start_page).to have_start_button
   end
 
   scenario "user will be able to start form" do

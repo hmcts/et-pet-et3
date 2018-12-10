803f1368d0184f0d2e0051a0ae16e51b9ecd42e0
Sivvoy Taing <sivvo@yahoo.com>
2018-12-10 13:44:25 +0000

    refactor test to ready from factory_bot symbol

--- spec/features/check_for_javascript_browser_errors_spec.rb
+++ spec/features/check_for_javascript_browser_errors_spec.rb
@@ -71,8 +71,16 @@ RSpec.feature "Check for Javascript Browser Errors", js: true, driver: :chromedr
 
   scenario "form submission page" do
     given_i_am
-    answer_all_questions
-    confirmation_of_supplied_details_page.submit_form
+    start_a_new_et3_response
+    answer_respondents_details
+    answer_claimants_details
+    answer_earnings_and_benefits
+    answer_defend_claim_question
+    answer_representative
+    answer_disability_question
+    answer_employers_contract_claim
+    answer_additional_information
+    confirmation_of_supplied_details
 
     expect(form_submission_page.js_severe_errors).to be_empty
   end

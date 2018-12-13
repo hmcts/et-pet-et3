d4b99477b982b3bf6b01e47e1cd409a363b41159
Sivvoy Taing <sivvo@yahoo.com>
2018-12-13 10:41:23 +0000

    start page should now work in english and in welsh

--- spec/features/access_start_page_spec.rb
+++ spec/features/access_start_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Access Start Page", js: true do
   let(:respondents_details_page) { ET3::Test::RespondentsDetailsPage.new }
 
   scenario "user will be able to read text" do
-    start_page.load(locale: current_locale_parameter)
+    load_landing_page
 
     expect(start_page).to have_header
     expect(start_page).to have_description
@@ -29,7 +29,7 @@ RSpec.feature "Access Start Page", js: true do
   end
 
   scenario "user will be able to start form" do
-    start_page.load(locale: current_locale_parameter)
+    start_page.load
 
     start_page.next
 

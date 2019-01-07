51f8e12f43657ba9bd6ab259a5d3a3edd978b5f6
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-01-07 17:02:02 +0000

    Reintroduce value getters to E&B spec

--- spec/features/fill_in_earnings_and_benefits_page_spec.rb
+++ spec/features/fill_in_earnings_and_benefits_page_spec.rb
@@ -32,18 +32,10 @@ RSpec.feature "Fill in Earnings and Benefits Page", js: true do
 
     expect(earnings_and_benefits_page).to be_displayed
     user = @claimant
-    # expect(earnings_and_benefits_page.agree_with_claimants_hours_question.get).to eql user.agree_with_claimants_hours
-    expect(earnings_and_benefits_page.agree_with_claimants_hours_question.queried_hours.field.value.to_f).to eql user.queried_hours.to_f
-    # expect(earnings_and_benefits_page.agree_with_earnings_details_question.get).to eql user.agree_with_earnings_details
-    # expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_pay_before_tax.field.value).to eql user.queried_pay_before_tax.to_f
-    expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_pay_before_tax.monthly.has_checked_field?).to be true if user.queried_pay_before_tax_period == "Monthly"
-    expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_pay_before_tax.weekly.has_checked_field?).to be true if user.queried_pay_before_tax_period == "Weekly"
-    expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_take_home_pay.field.value).to eql user.queried_take_home_pay.to_f if user.agree_with_earnings_details_question == "Yes"
-    expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_take_home_pay.monthly.has_checked_field?).to be true if user.queried_take_home_pay_period == "Monthly"
-    expect(earnings_and_benefits_page.agree_with_earnings_details_question.queried_take_home_pay.weekly.has_checked_field?).to be true if user.queried_take_home_pay_period == "Weekly"
-    # expect(earnings_and_benefits_page.agree_with_claimant_notice_question.get).to eql user.agree_with_claimant_notice
-    expect(earnings_and_benefits_page.agree_with_claimant_notice_question.disagree_claimant_notice_reason.root_element.value).to eql user.disagree_claimant_notice_reason if user.agree_with_claimant_notice == "No"
-    # expect(earnings_and_benefits_page.agree_with_claimant_pension_benefits_question.get).to eql user.agree_with_claimant_pension_benefits
-    expect(earnings_and_benefits_page.agree_with_claimant_pension_benefits_question.disagree_claimant_pension_benefits_reason.root_element.value).to eql user.disagree_claimant_pension_benefits_reason if user.agree_with_claimant_pension_benefits == "No"
+
+    earnings_and_benefits_page.agree_with_claimants_hours_question.assert_answer_for(user)
+    earnings_and_benefits_page.agree_with_earnings_details_question.assert_answer_for(user)
+    earnings_and_benefits_page.agree_with_claimant_notice_question.assert_answer_for(user)
+    earnings_and_benefits_page.agree_with_claimant_pension_benefits_question.assert_answer_for(user)
   end
 end

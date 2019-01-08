a4214cd3a5bea1d3ee9017a1975213fdb7fad26a
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-01-08 09:51:18 +0000

    Refactor your_rep page object to pass spec

--- test_common/page_objects/your_representative_page.rb
+++ test_common/page_objects/your_representative_page.rb
@@ -3,20 +3,22 @@ module ET3
     class YourRepresentativePage < BasePage
       set_url '/respond/your_representative'
       element :header, :content_header, 'your_representative.header'
+
       section :have_representative_question, :single_choice_option, 'questions.have_representative.label', exact: true do
         include ET3::Test::I18n
-        element :yes, :gds_multiple_choice_option, 'questions.have_representative.yes.label' do
-          element :selector, :css, 'input[type="radio"]'
-          def set(*args); selector.set(*args); end
-        end
-        element :no, :gds_multiple_choice_option, 'questions.have_representative.no.label' do
-          element :selector, :css, 'input[type="radio"]'
-          def set(*args); selector.set(*args); end
-        end
+        element :yes, :gds_multiple_choice_option, 'questions.have_representative.yes.label'
+        element :no, :gds_multiple_choice_option, 'questions.have_representative.no.label'
+
         def set_for(user_persona)
           choose(factory_translate(user_persona.have_representative), name: 'your_representative[have_representative]')
         end
+
+        def assert_answer(untranslated_symbol)
+          find(:gds_multiple_choice_option, untranslated_symbol)
+              .assert_selector(:field, nil, checked: true)
+        end
       end
+
       element :continue_button, :submit_text, 'components.save_and_continue_button'
       def next
         continue_button.click

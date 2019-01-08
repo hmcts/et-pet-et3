1c5238172d1d5a2b2d563473942f926b7952efc2
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-01-08 11:07:46 +0000

    Refactor disability page object to pass spec

--- test_common/page_objects/disability_page.rb
+++ test_common/page_objects/disability_page.rb
@@ -4,27 +4,42 @@ module ET3
       set_url '/respond/disability'
       element :header, :content_header, 'disability.header'
       element :error_header, :error_titled, 'errors.header', exact: true
+
       section :disability_question, :single_choice_option, 'questions.disability.label', exact: false do
         include ET3::Test::I18n
-        element :yes, :gds_multiple_choice_option, 'questions.disability.yes.label' do
-          element :selector, :css, 'input[type="radio"]'
-          def set(*args); selector.set(*args); end
-        end
-        element :no, :gds_multiple_choice_option, 'questions.disability.no.label' do
-          element :selector, :css, 'input[type="radio"]'
-          def set(*args); selector.set(*args); end
-        end
+
+        element :yes, :gds_multiple_choice_option, 'questions.disability.yes.label'
+        element :no, :gds_multiple_choice_option, 'questions.disability.no.label'
+
         section :disability_information, :textarea_labelled, 'questions.disability.disability_information.label', exact: :false do
           delegate :set, to: :root_element
         end
+
         element :error_too_long, :exact_error_text, 'errors.messages.too_long', exact: false
+
         def set_for(user_persona)
           choose(factory_translate(user_persona.disability), name: 'disability[disability]')
           if t(user_persona.disability) == t('questions.disability.yes.label') && user_persona.disability_information != nil
             disability_information.set(user_persona.disability_information)
           end
         end
+
+        def assert_answers_for(user_persona)
+          find(:gds_multiple_choice_option,
+               user_persona.disability).assert_selector(:field, nil, checked: true) &&
+
+          if t(user_persona.disability) == t('questions.disability.yes.label')
+            root_element.
+                assert_selector(
+                    :field,
+                    t('questions.disability.disability_information.label'),
+                    with: user_persona.disability_information,
+                    exact: false
+                )
+          end
+        end
       end
+
       element :continue_button, :submit_text, 'components.save_and_continue_button'
       def next
         continue_button.click

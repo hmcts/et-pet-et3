20f9a8027d82b739f213d9af0882f1fcd4bf6e0a
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-01-10 11:26:15 +0000

    Add method to intercept enforced capitalisation of attribute names

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -1,4 +1,15 @@
 class BaseForm < ApplicationRecord
   establish_connection adapter: :nulldb,
                        schema: 'config/nulldb_schema.rb'
+
+  def self.human_attribute_name(attribute_name, *)
+    translated_attribute = catch :exception do
+      I18n.translate("attributes.#{attribute_name}", throw: true)
+    end
+    if translated_attribute.is_a?(I18n::MissingTranslation)
+      super
+    else
+      translated_attribute
+    end
+  end
 end

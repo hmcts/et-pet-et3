b1e39c1d0adf1e00ac5c435105125b0da7b6a6f2
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-01-21 10:29:03 +0000

    Remove expiration and json pages from localised namespace

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
       get "form_submission", to: "form_submissions#index"
     end
     root 'static_pages#index'
-    get "/session_expired" => 'static_pages#expired'
-    get "/ping" => "status#ping"
-    get "/healthcheck" => "status#healthcheck"
     delete "/respond/confirmation_of_supplied_details/remove_rtf" => 'confirmation_of_supplied_details#destroy_rtf', as: :remove_rtf
     mount ApiProxy.new(backend: "#{ENV.fetch('ET_API_URL', 'http://api.et.127.0.0.1.nip.io:3100/api')}/v2/s3/create_signed_url", streaming: false), at: "/api/s3/create_signed_url"
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+  get "/session_expired" => 'static_pages#expired'
+  get "/ping" => "status#ping"
+  get "/healthcheck" => "status#healthcheck"
 end

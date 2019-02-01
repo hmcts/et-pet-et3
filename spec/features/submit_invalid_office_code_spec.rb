cbf1e97eb1156911a423bb42ff119eec911fcdd7
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-02-01 14:51:20 +0000

    Remove additional, unnecessary, steps from spec

--- spec/features/submit_invalid_office_code_spec.rb
+++ spec/features/submit_invalid_office_code_spec.rb
@@ -12,14 +12,6 @@ RSpec.feature "Submit invalid office code", js: true do
     respondents_details_page.load
     start_a_new_et3_response
     answer_respondents_details
-    answer_claimants_details
-    answer_earnings_and_benefits
-    answer_defend_claim_question
-    answer_representative
-    answer_disability_question
-    answer_employers_contract_claim
-    answer_additional_information
-    answer_confirmation_of_supplied_details
 
     expect(respondents_details_page).to be_displayed
     expect(respondents_details_page.case_number_question).to have_error_office_invalid

4902247898b1d41bee19c7c824f3a923f82d9f02
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-02-14 12:21:06 +0000

    Add a fake API service to mock office validation call in development

--- app/validators/remotely_validate_office_code_validator.rb
+++ app/validators/remotely_validate_office_code_validator.rb
@@ -1,6 +1,6 @@
 class RemotelyValidateOfficeCodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, :invalid_office) if
-        EtApiHandler.validate_response(attribute, value) == "invalid_office_code"
+        Rails.configuration.api_handler.validate_response(attribute, value) == "invalid_office_code"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Use real EtApiHandler in production
+  config.api_handler = EtApiHandler
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 

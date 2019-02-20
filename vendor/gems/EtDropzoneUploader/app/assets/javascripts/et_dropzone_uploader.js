14cc92700d89ea51c044b33a69af47c30b6eaab5
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-02-20 15:26:28 +0000

    Extract all JS assets into gem

--- vendor/gems/EtDropzoneUploader/app/assets/javascripts/et_dropzone_uploader.js
+++ vendor/gems/EtDropzoneUploader/app/assets/javascripts/et_dropzone_uploader.js
@@ -1,8 +1,8 @@
 // This is a manifest file that'll be compiled into et_dropzone_uploader.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
@@ -10,9 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require govuk/show-hide-content
-//= require rails-ujs
-//= require turbolinks
-//= require et_dropzone_uploader
-//= require_tree .
+//= require dropzone
+//= require et_dropzone_uploader/uploadAdditionalInformation

2f52c6e57b7ff8f8ccc4c588857e21afea4c8bea
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-03-07 14:46:39 +0000

    Update test to expect Welsh links in Welsh mode

--- spec/features/view_footer_spec.rb
+++ spec/features/view_footer_spec.rb
@@ -11,67 +11,67 @@ RSpec.feature "View Footer", js: true do
   scenario "on start page" do
     start_page.load(locale: current_locale_parameter)
 
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on respondent's details page" do
     respondents_details_page.load(locale: current_locale_parameter)
 
-    expect(respondents_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(respondents_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on claimant's details page" do
     claimants_details_page.load(locale: current_locale_parameter)
 
-    expect(claimants_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(claimants_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on earnings and benefits page" do
     earnings_and_benefits_page.load(locale: current_locale_parameter)
 
-    expect(earnings_and_benefits_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(earnings_and_benefits_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on response page" do
     response_page.load(locale: current_locale_parameter)
 
-    expect(response_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(response_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on your representative page" do
     your_representative_page.load(locale: current_locale_parameter)
 
-    expect(your_representative_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(your_representative_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on your representative's details page" do
     your_representatives_details_page.load(locale: current_locale_parameter)
 
-    expect(your_representatives_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(your_representatives_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on disability page" do
     disability_page.load(locale: current_locale_parameter)
 
-    expect(disability_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(disability_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on employer contract claim page" do
     employers_contract_claim_page.load(locale: current_locale_parameter)
 
-    expect(employers_contract_claim_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(employers_contract_claim_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on additional information page" do
     additional_information_page.load(locale: current_locale_parameter)
 
-    expect(additional_information_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(additional_information_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on confirmation of supplied details page" do
     confirmation_of_supplied_details_page.load(locale: current_locale_parameter)
 
-    expect(confirmation_of_supplied_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(confirmation_of_supplied_details_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
   scenario "on form submission page" do
@@ -87,7 +87,7 @@ RSpec.feature "View Footer", js: true do
     answer_additional_information
     answer_confirmation_of_supplied_details
 
-    expect(form_submission_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path)
+    expect(form_submission_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
   end
 
 end

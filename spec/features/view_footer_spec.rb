810c9c80f8fbbcd35ffe679f2104710745847bf0
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-04-16 14:03:50 +0100

    Use shared examples in footer spec

--- spec/features/view_footer_spec.rb
+++ spec/features/view_footer_spec.rb
@@ -8,92 +8,14 @@ RSpec.feature "View Footer", js: true do
     stub_build_blob_to_azure
   end
 
-  scenario "on start page" do
-    start_page.load(locale: current_locale_parameter)
+  shared_examples "on a per-page basis" do
+    scenario "will show footer links" do
+      current_page.load(locale: current_locale_parameter)
 
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on respondent's details page" do
-    respondents_details_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on claimant's details page" do
-    claimants_details_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on earnings and benefits page" do
-    earnings_and_benefits_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on response page" do
-    response_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on your representative page" do
-    your_representative_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on your representative's details page" do
-    your_representatives_details_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on disability page" do
-    disability_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on employer contract claim page" do
-    employers_contract_claim_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on additional information page" do
-    additional_information_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
-  end
-
-  scenario "on confirmation of supplied details page" do
-    confirmation_of_supplied_details_page.load(locale: current_locale_parameter)
-
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
+      expect(current_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
+      expect(current_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
+      expect(current_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
+    end
   end
 
   scenario "on form submission page" do
@@ -109,9 +31,76 @@ RSpec.feature "View Footer", js: true do
     answer_additional_information
     answer_confirmation_of_supplied_details
 
-    expect(start_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
-    expect(start_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
+    expect(form_submission_page).to have_link(t('components.footer.cookies'), href: cookies_path(locale: current_locale_parameter))
+    expect(form_submission_page).to have_link(t('components.footer.privacy'), href: privacy_notice_path(locale: current_locale_parameter))
+    expect(form_submission_page).to have_link(t('components.footer.terms'), href: terms_and_conditions_path(locale: current_locale_parameter))
+  end
+
+
+  context "when originally on the start page" do
+    let(:current_page) { ET3::Test::StartPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the respondent's details page" do
+    let(:current_page) { ET3::Test::RespondentsDetailsPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the claimant's details page" do
+    let(:current_page) { ET3::Test::ClaimantsDetailsPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the earnings and benefits page" do
+    let(:current_page) { ET3::Test::EarningsAndBenefitsPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the response page" do
+    let(:current_page) { ET3::Test::ResponsePage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the your representative page" do
+    let(:current_page) { ET3::Test::ResponsePage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the your representative's details page" do
+    let(:current_page) { ET3::Test::ResponsePage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the disability page" do
+    let(:current_page) { ET3::Test::DisabilityPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the employer contract claim page" do
+    let(:current_page) { ET3::Test::EmployersContractClaimPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the additional information page" do
+    let(:current_page) { ET3::Test::AdditionalInformationPage.new }
+
+    include_examples "on a per-page basis"
+  end
+
+  context "when originally on the confirmation of supplied details page" do
+    let(:current_page) { ET3::Test::ConfirmationOfSuppliedDetailsPage.new }
+
+    include_examples "on a per-page basis"
   end
 
 end

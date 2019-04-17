6dfa90c13f48efbfe49c9561359e095941445bcf
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-04-17 09:25:00 +0100

    RST-1836: Clear Existing Response Data When Starting New Session (#228)
    * Create spec to ensure a new session will overwrite existing data
    
    * Refactor clearing session data into an application controller method
    
    * Update 'Start now' button to clear session data before redirecting
    
    * Refactor test to use assert_selector and improve performance

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,9 @@ class StaticPagesController < ApplicationController
   def index; end
 
   def expired; end
+
+  def start_new_session
+    clear_session_data
+    redirect_to edit_respondents_details_path
+  end
 end

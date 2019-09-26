3e5c17702fe4f9e9a31160cb258a38f2ed0cd8eb
frederic jonquieres <fjonquieres83@gmail.com>
2019-09-26 13:52:41 +0100

    Bug fix for test

--- spec/features/view_sidebar_spec.rb
+++ spec/features/view_sidebar_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "View Sidebar", js: true do
   include ET3::Test::I18n
 
   scenario "start page" do
-    start_page.load
+    start_page.load(locale: current_locale_parameter)
 
     expect(start_page.sidebar).to have_link(t('components.sidebar.claim_link'), href: t('components.sidebar.claim_href'))
     expect(start_page.sidebar).to have_link(t('components.sidebar.response_link'), href: t('components.sidebar.response_href'))

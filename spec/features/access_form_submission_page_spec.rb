5bed9a7bb675c642deccb2b605a28d7c1fed7af8
frederic jonquieres <fjonquieres83@gmail.com>
2019-12-18 10:19:39 +0000

    Changes for tests

--- spec/features/access_form_submission_page_spec.rb
+++ spec/features/access_form_submission_page_spec.rb
@@ -23,7 +23,6 @@ RSpec.feature "Access Form Submission Page", js: true do
     expect(form_submission_page).to be_displayed
     expect(form_submission_page).to have_submission_confirmation
     expect(form_submission_page).to have_reference_number
-    expect(form_submission_page).to have_copy_sent
     expect(form_submission_page).to have_office_contact
     expect(form_submission_page).to have_submission_date
     expect(form_submission_page).to have_copy_sent

090800a4c44d78f308a9493d6ba583822fac6598
frederic jonquieres <fjonquieres83@gmail.com>
2021-06-08 13:06:26 +0100

    RST-3273 access_privacy_spec & fill_in_whole_form_spec & Cookies_spec

--- spec/support/shared_examples/google_tag_manager.rb
+++ spec/support/shared_examples/google_tag_manager.rb
@@ -29,7 +29,7 @@ RSpec.shared_examples 'google tag manager' do |page_object_class:|
         Rails.configuration.google_tag_manager_account = old
       end
       # @TODO RST-3332
-      xit 'has no google tag manager script or noscript tags' do
+      it 'has no google tag manager script or noscript tags' do
         expect(page_object).to be_displayed.and have_no_google_tag_manager_sections
       end
     end

8da5dd82eade404c9d588175a941cff06df57963
frederic jonquieres <fjonquieres83@gmail.com>
2022-03-01 15:43:10 +0000

    RST-3739 Rspec for the logout on start page

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,4 +1,4 @@
 require 'webmock/rspec'
 app_host = ENV.fetch('CAPYBARA_SERVER_HOST', ENV.fetch('HOSTNAME', `hostname`.strip))
 storage_host = URI.parse(ENV.fetch('AZURE_STORAGE_BLOB_HOST', 'http://azure_blob_storage.et.127.0.0.1.nip.io:1000')).host
-WebMock.disable_net_connect!(allow: [app_host, 'chromedriver.storage.googleapis.com', storage_host, 'github.com'], allow_localhost: true)
+WebMock.disable_net_connect!(allow: [app_host, 'chromedriver.storage.googleapis.com', storage_host, 'github.com', 'objects.githubusercontent.com'], allow_localhost: true)

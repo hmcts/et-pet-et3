2563e7468026a9215505d890e75060192df90ddb
Gary Taylor <gary.taylor@hismessages.com>
2022-04-20 09:23:27 +0100

    RST-3083 GDS Upgrade - Data migration for file uploads

---
+++ db/migrate/20220420071544_migrate_uploaded_files_to_new_format.rb
@@ -0,0 +1,30 @@
+class MigrateUploadedFilesToNewFormat < ActiveRecord::Migration[6.0]
+  class Store < ActiveRecord::Base
+    self.table_name = :stores
+    serialize :hash_store
+    serialize :api_response
+  end
+
+  def up
+    Store.where("hash_store ~ E':upload_file_name: [^\\\']+$' OR hash_store ~ E':upload_file_name: \\\'.+\\\''").where(api_response: nil).each do |store|
+      path = store.hash_store.dig(:additional_information_answers, :upload_additional_information)
+      filename = store.hash_store.dig(:additional_information_answers, :upload_file_name)
+      announce("Upgrading store id #{store.id}")
+      next if path.nil? || filename.nil?
+
+      new_value = {
+                    path: path,
+                    filename: filename,
+                    content_type: 'application/rtf'
+                  }.with_indifferent_access
+      store.hash_store[:additional_information_answers][:upload_additional_information] = new_value
+      store.hash_store[:additional_information_answers].delete(:upload_file_name)
+      store.save(touch: false)
+      announce("Upgraded store id #{store.id}")
+    end
+  end
+
+  def down
+
+  end
+end

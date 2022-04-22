d848b2cd07c14a3311c7cbeab04ae0f5a4ec5bea
Gary Taylor <gary.taylor@hismessages.com>
2022-04-22 18:05:34 +0100

    RST-3083 Fixed autoload issues

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,4 +1,4 @@
-ActiveSupport.on_load(:after_initialize) do
+Rails.application.config.to_prepare do
   ActiveRecord::Type.register(:currency_float, CurrencyFloatType)
   ActiveRecord::Type.register(:et_date, EtDateType)
 end

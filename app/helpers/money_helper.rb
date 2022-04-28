2a89cb91f7c5acbf4fa063338ae0f98640eb55c7
Gary Taylor <gary.taylor@hismessages.com>
2022-04-28 07:24:23 +0100

    RST-3083 Fix for empty money values in confirmation of supplied details

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,6 +1,6 @@
 module MoneyHelper
   def money_in_pounds(value)
-    return nil if value.nil? || value.empty?
+    return nil if value.nil? || value == ''
 
     number_to_currency(value, unit: '&pound;'.html_safe)
   end

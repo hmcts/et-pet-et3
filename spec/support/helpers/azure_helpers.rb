3767ceeb0f496230aa9d2ab4883ff8482c0e9dc5
Gary Taylor <gary.taylor@hismessages.com>
2022-05-17 08:51:14 +0100

    RST-885 Refactored tests

--- spec/support/helpers/azure_helpers.rb
+++ spec/support/helpers/azure_helpers.rb
@@ -84,3 +84,11 @@ module ET3
     end
   end
 end
+
+RSpec.configure do |c|
+  c.before(:suite) do
+
+    ET3::Test::AzureHelpers.configure_test_container
+    ET3::Test::AzureHelpers.configure_cors
+  end
+end

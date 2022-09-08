b5c3e38ea0f510a81b7b223d3de5af4685156a2d
kalvinlyu <kalvin.lyu@solirius.com>
2022-09-08 10:38:17 +0100

    RST-4500
    Changed spec to hide textbox so it would pass

--- spec/features/fill_in_earnings_and_benefits_page_spec.rb
+++ spec/features/fill_in_earnings_and_benefits_page_spec.rb
@@ -55,16 +55,17 @@ RSpec.feature "Fill in Earnings and Benefits Page", js: true do
         expect(p.queried_pay_before_tax_period.value).to eql t(user.queried_pay_before_tax_period)
         expect(p.queried_take_home_pay.value).to eql sprintf('%.2f', user.queried_take_home_pay)
         expect(p.queried_take_home_pay_period.value).to eql t(user.queried_take_home_pay_period)
-
       end
       expect(p.agree_with_claimant_notice_question.value).to eql(t(user.agree_with_claimant_notice))
-      if user.agree_with_claimant_notice.to_s.split('.').last == 'no' and user.agree_with_claimant_pension_benefits.to_s.split('.').last == 'yes'
+      if user.agree_with_claimant_notice.to_s.split('.').last == 'no'
         expect(p.disagree_claimant_notice_reason.value).to eql user.disagree_claimant_notice_reason
       end
+      earnings_and_benefits_page.agree_with_claimant_notice_question.set(:yes)
       expect(p.agree_with_claimant_pension_benefits_question.value).to eql(t(user.agree_with_claimant_pension_benefits))
       if user.agree_with_claimant_pension_benefits.to_s.split('.').last == 'no'
         expect(p.disagree_claimant_pension_benefits_reason.value).to eql user.disagree_claimant_pension_benefits_reason
       end
+      earnings_and_benefits_page.agree_with_claimant_notice_question.set(:no)
     end
   end
 end

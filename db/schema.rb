89c31528502eb521590e9b39299d93eaaa5f5c30
garytaylor <gary.taylor@hismessages.com>
2023-04-24 07:11:50 +0100

    (RST-5116) Rails upgraded to 7.0.4.3 (#583)

--- db/schema.rb
+++ db/schema.rb
@@ -2,24 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2022_04_20_071544) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_071544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stores", force: :cascade do |t|
     t.string "uuid"
     t.text "hash_store", default: "--- {}\n"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "api_response"
     t.integer "user_id"
     t.string "last_path", default: "/respond/respondents_details", null: false
@@ -32,9 +31,9 @@ ActiveRecord::Schema[6.1].define(version: 2022_04_20_071544) do
     t.string "encrypted_password", default: "", null: false
     t.string "reference", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["reference"], name: "index_users_on_reference", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

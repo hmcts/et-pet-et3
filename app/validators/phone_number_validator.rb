3e3fe5270b7031a64dfba29c94a5267f879c36ac
garytaylor <gary.taylor@hismessages.com>
2023-05-30 12:38:42 +0100

    (RST-5157) Ruby 3 upgrade - fixes to keyword argument issues

--- app/validators/phone_number_validator.rb
+++ app/validators/phone_number_validator.rb
@@ -1,5 +1,5 @@
 class PhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid_phone_number) unless value =~ /\A(\+?)((\d(\ ?)(\-?)){7,})\z/
+    record.errors.add(attribute, :invalid_phone_number) unless value =~ /\A(\+?)((\d(\ ?)(\-?)){7,30})\z/
   end
 end

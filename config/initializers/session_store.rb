9927fa8e449b9fa231628b1767b734da31642036
garytaylor <gary.taylor@hismessages.com>
2023-09-15 09:56:47 +0100

    (RST-5891) Secured cookies (#616)

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+Rails.application.config.session_store :cookie_store,
+                                       key: '_et3_session',
+                                       secure: Rails.application.config.secure_cookies

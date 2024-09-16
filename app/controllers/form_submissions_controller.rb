10ba3a84a60aeab323fc1b6559d70b3a669f1c4a
garytaylor <gary.taylor@hismessages.com>
2024-09-16 06:41:05 +0100

    Rst 6837 - Updated confirmation page for use with new API (#673)
    * (RST-6837) WIP
    
    * (RST-6837) New final confirmation page

--- app/controllers/form_submissions_controller.rb
+++ app/controllers/form_submissions_controller.rb
@@ -1,6 +1,7 @@
 class FormSubmissionsController < ApplicationController
   skip_before_action :set_start_session_timer
   before_action :disable_save_and_return
+  helper_method :current_store
 
   def index
     @reference_number = current_store.api_response[:data]["meta"]["BuildResponse"]["reference"]
@@ -11,6 +12,7 @@ class FormSubmissionsController < ApplicationController
     @office_email = current_store.api_response[:data]["meta"]["BuildResponse"]["office_email"]
     @office_phone_number = current_store.api_response[:data]["meta"]["BuildResponse"]["office_phone_number"]
     @respondent_email = current_store.hash_store.dig(:respondents_detail_answers, :email_address)
+    @additional_information_file = current_store.hash_store.dig(:additional_information_answers, :upload_additional_information, :filename)
     clear_session_data if current_store.api_response[:status] == 202
   end
 
--- spec/support/page_objects/form_submission_page.rb
+++ spec/support/page_objects/form_submission_page.rb
@@ -5,9 +5,8 @@ module ET3
       element :submission_confirmation, :element_with_text, 'submission.confirmation'
       element :reference_number, :element_with_text, 'submission.reference'
       element :copy_sent, :paragraph_with_text, 'submission.copy_sent', exact: false
-      element :copy_sent_without_email, :paragraph_with_text, 'submission.copy_sent_without_email', exact: false
       element :office_contact, :element_with_text, 'submission.office_contact'
-      element :submission_date, :element_with_text, 'submission.date', exact: false
+      element :submission_date, :summary_list_row_labelled, 'submission.date', exact: false
       element :valid_pdf_download, :link_named, 'links.form_submission.valid_pdf_download'
       element :invalid_pdf_download, :link_named, 'links.form_submission.invalid_pdf_download'
       element :return_to_govuk_button, :link_named, 'submission.return_link'
@@ -18,8 +17,6 @@ module ET3
       def assert_copy_sent(respondent)
         if respondent.contact_preference.to_s.split('.')[-2] == 'email'
           expect(copy_sent).to have_text(respondent.email_address)
-        else
-          expect(self).to have_copy_sent_without_email
         end
       end
     end

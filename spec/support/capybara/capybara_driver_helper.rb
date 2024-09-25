ffa720f0e8faef1e71191b8060907ba8fcd14235
garytaylor <gary.taylor@hismessages.com>
2024-09-25 10:09:06 +0100

    (RST-6875) Welsh content for submission page (#675)
    * (RST-6875) Welsh content for submission page
    
    * (RST-6875) Corrected a couple of translation issues
    
    * (RST-6875) Corrected a couple more translation issues

--- spec/support/capybara/capybara_driver_helper.rb
+++ spec/support/capybara/capybara_driver_helper.rb
@@ -22,6 +22,8 @@ Capybara.register_driver :chrome_local do |app|
   options.add_argument('--no-sandbox')
   options.add_argument('--headless=new')
   options.add_argument('--lang=en-GB')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 

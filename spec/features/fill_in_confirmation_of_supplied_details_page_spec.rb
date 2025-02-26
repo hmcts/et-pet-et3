8526ede6380786a15305b00bc858d7fa36f78988
garytaylor <gary.taylor@hismessages.com>
2025-02-26 11:54:35 +0000

    (#RST-7076) New question for case heard by (judge or panel)

--- spec/features/fill_in_confirmation_of_supplied_details_page_spec.rb
+++ spec/features/fill_in_confirmation_of_supplied_details_page_spec.rb
@@ -16,6 +16,7 @@ RSpec.feature "Fill in Confirmation of Supplied Details Page", :js do
     start_a_new_et3_response
     registration_start
     answer_respondents_details
+    answer_case_heard_by
     answer_claimants_details
     answer_earnings_and_benefits
     answer_defend_claim_question
@@ -37,6 +38,7 @@ RSpec.feature "Fill in Confirmation of Supplied Details Page", :js do
     start_a_new_et3_response
     registration_start
     answer_respondents_details
+    answer_case_heard_by
     answer_claimants_details
     answer_earnings_and_benefits
     answer_defend_claim_question
@@ -66,6 +68,7 @@ RSpec.feature "Fill in Confirmation of Supplied Details Page", :js do
     start_a_new_et3_response
     registration_start
     answer_respondents_details
+    answer_case_heard_by
     answer_claimants_details
     answer_earnings_and_benefits
     answer_defend_claim_question

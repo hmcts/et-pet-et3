055ae0fab044c25c0e10094996ab4ba58d003b0c
Gary Taylor <gary.taylor@hismessages.com>
2026-04-08 21:56:39 +0100

    (rst-8007) Converted inline scripts to stimulus controllers

---
+++ app/javascript/entrypoints/controllers/additional_information_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus";
+
+import { Components } from "et_gds_design_system";
+const { DropzoneUploader } = Components;
+
+export default class extends Controller {
+  connect() {
+    DropzoneUploader.init();
+  }
+}
--- app/javascript/entrypoints/pages/FormSubmissionPage.js
+++
@@ -1,5 +0,0 @@
-import DisablePdfLink from "../components/DisablePdfLink";
-
-export default function FormSubmissionPage() {
-  DisablePdfLink()
-}

59830dc2493dd1ff20f3f000cecc8be68e7ac966
garytaylor <gary.taylor@hismessages.com>
2026-06-03 08:10:55 +0100

    (rst-7394) Upgrade sentry gems and insert extras into data (#746)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,4 +7,25 @@ Sentry.init do |config|
 
     config.release = version
   end
+
+  config.before_send = lambda do |event, _hint|
+    # Add response context for ActiveJob errors
+    if event.extra[:active_job] && event.extra[:arguments]&.first&.start_with?('gid://app/Store/')
+      begin
+        store = GlobalID::Locator.locate(event.extra[:arguments].first)
+        if store
+          event.user = { id: claim.user.id } if claim.user
+          event.extra.merge!(
+            release: Rails.application.config.app_version,
+            userId: current_user&.id,
+            storeId: store&.id,
+            caseNumber: store&.hash_store&.dig(:respondents_detail_answers, :case_number)
+          )
+        end
+      rescue StandardError
+        # Ignore errors locating the claim
+      end
+    end
+    event
+  end
 end

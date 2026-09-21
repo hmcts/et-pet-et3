ee483afd21980bbf92750a5d9fac675212eb47b5
Gary Taylor <gary.taylor@hismessages.com>
2026-09-21 09:15:41 +0100

    (rst-7076) Bug fix - feature flag was being used in smoke test which will fail

--- spec/support/feature_flags.rb
+++ spec/support/feature_flags.rb
@@ -1,5 +1,7 @@
 RSpec.configure do |c|
-  c.before do
+  c.before do |example|
+    next if example.metadata[:smoke]
+
     FeatureFlag.find_or_create_by!(key: 'era_oct_26', name: 'ERA Oct 2026', default_value: true)
   end
 end
